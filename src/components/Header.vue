3bbb3a5fbd26b78e9cd3fecb4ae1c832c6273da9
painteyes <andreantoniocchipinti@gmail.com>
2022-01-17 13:10:51 +0100

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+    <header>
+
+        <div class="container">
+
+            <div class="logo">
+                <img :src="require('../assets/images/logotype.png')" alt="">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li v-for="element,index in menuItems" :key='index'>{{element}}</li>
+                </ul>
+            </div>
+
+        </div>
+
+    </header>
+</template>
+
+
+<script>
+export default {
+
+    name: 'Header',
+
+    props: {
+        menuItems: Array,
+
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+@import "../styles/general.scss";
+
+header{
+    height: 50px;    
+    width: 100%;
+    position: absolute;
+    
+    .container{
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .logo {
+            width: 100px;
+
+            img{
+                display: block;
+            }
+        }
+
+        .menu {
+            color: white;
+            height: 100%;
+            display: flex;
+            align-items: flex-end;
+
+            ul {
+                display: flex;
+            
+                li {
+                    margin: 0 30px;
+                    text-transform: uppercase;
+                    line-height: 50px;
+
+                    &:hover {
+                        border-top: 4px solid white ;
+                        line-height: 46px;
+                        cursor: pointer;
+                    }
+                }
+
+                :first-child {
+                    margin-left: 0;
+                }
+
+                :last-child {
+                    margin-right: 0;
+                }
+            
+            }
+        }
+    }
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,61 @@
+<template>
+<main>
+    <div class="jumbotron">
+
+        <div class="explore">
+            
+        </div>
+
+        <img :src="require('../assets/images/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg')" alt="">
+
+        
+
+    </div>
+
+</main>    
+</template>
+
+
+<script>
+export default {
+    name: 'Main',
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+main{
+
+    
+
+
+
+}
+
+.jumbotron {
+
+
+    
+    height: 900px;
+    display: flex;
+
+    img{
+        width: 70%;
+    }
+
+    .explore {
+        background-color: #1f1e38;
+        width: 30%;
+        height: 100%;
+    }
+
+
+
+}
+
+
+
+
+
+</style>
